39b1f5bbc5dd54ebb29458409c41b82c3165a6e9
Gaston Ramirez <gastonframirez@gmail.com>
2017-06-26 01:05:58 -0300

    Agrego #3.
    Falta ver #4 (de donde sacar los datos) y controlar que este todo lo necesario en #3.

--- assets/js/objetos-tm.js
+++ assets/js/objetos-tm.js
@@ -1,17 +1,37 @@
+function dibujarD3_objeto(catIngresos) {
+  $("#objetos-tm").empty();
   d3.json("assets/data/por-objeto-del-gasto/presupuesto-por-objeto-del-gasto.json", function(err, res) {
     if (!err) {
-        // console.log(res);
-        var data = d3.nest()
-          .key(function(d) { return d.rec1; })
-          .key(function(d) { return d.rec2; })
-          .key(function(d) { return d.rec3; })
-          .key(function(d) { return d.rec4; })
-          .key(function(d) { return d.rec5; })
-          .entries(res);
+        console.log(res);
+          var data = d3.nest()
+                      .key(function(d) { return d.rec1; })
+                      .key(function(d) { return d.rec2; })
+                      .key(function(d) { return d.rec3; })
+                      .key(function(d) { return d.rec4; })
+                      .key(function(d) { return d.rec5; })
+                      .entries(res);
 
+
+        function getTooltipWidth(){
+          var wWidth = $( window ).width();
+          if (wWidth < 480){
+            return $( window ).width()-50;
+          }else if(wWidth < 768){
+            return 500;
+          }else if(wWidth < 1200){
+            return 700;
+          }else{
+            return 700;
+          }
+        }
         // console.log(data);
+        $("#ingresos-title").html("Gastos por objeto");
         var visualization = d3plus.viz()
           .container("#objetos-tm")
+          .legend({"size": 30})
+          .labels({"align": "left", "valign": "top"})
+          .tooltip(true)
+          .tooltip({"large":getTooltipWidth(), "small":getTooltipWidth()})
           .data(res)
           .type("tree_map")
           .id(["rec1", "rec2", "rec3", "rec4", "rec5", "key"])
@@ -20,8 +40,8 @@
           .format({
               "number": function(number, key) {
   		          var formatted = d3plus.number.format(number, key)
-  		          if (key.key === "valor") {
-  		        	     return "$" + formatted.replace("B", " Mm")
+  		          if (key.key === "value") {
+  		        	     return "$" + formatted.replace("B", " Mm");
   		          }
   		          else {
   		            return formatted
@@ -31,3 +51,6 @@
           .draw();
     }
     });
+}
+
+dibujarD3_objeto(0);
