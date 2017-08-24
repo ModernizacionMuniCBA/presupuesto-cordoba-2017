46ecf72496dd25cfbc900ecf5b050c5d63e4c90b
gastonframirez <gastonframirez@gmail.com>
2017-08-24 12:23:58 -0300

    Cambios en js
    Agrego cambio de hash en tabs.
    Agrego que si el hash esta presente en la url se muestre lo
    correspondiente a ese hash.
    Merge de afectacionEspecifica.js con ingresosGrapher.js

--- assets/js/ingresosGrapher.js
+++ assets/js/ingresosGrapher.js
@@ -80,9 +80,203 @@ function llenarTablas_ing(){
 });
 }
 
+function dibujarD3_AE() {
+  $("#afectacion-especifica-j").empty();
+  d3.json("assets/data/ingresos/recursos-afectacion-totales-presupuesto-2017.json", function(err, res) {
+    if (!err) {
+        // console.log(res);
+
+        var data = d3.nest()
+                    // .key(function(d) { return d.rec1; })
+                    .entries(res);
+
+        console.log(data);
+
+        var visualization = d3plus.viz()
+          .container("#afectacion-especifica-j")
+          .background("#EEEEEE")
+           .legend({"size": 50})
+          // .labels({"align": "left", "valign": "top"})
+          .tooltip(true)
+          .tooltip({"children":0})
+          // .tooltip({"large":getTooltipWidth(), "small":getTooltipWidth()})
+          .data(res)
+          .type("pie")
+          .id(["key"])
+          .size("valor")
+          .format("es_ES")
+          .format({
+              "number": function(number, key) {
+  		          var formatted = d3plus.number.format(number, key);
+  		          if (key.key === "valor") {
+                    var formatted = number.toLocaleString("es-AR")
+                    return "$" + formatted;
+  		          }
+  		          else {
+  		            return formatted
+  		          }
+  		        }
+  				})
+          .draw();
+    }
+    });
+    $("#afectacion-especifica-jm").empty();
+    d3.json("assets/data/ingresos/recursos-afectacion-presupuesto-2017.json", function(err, res) {
+      if (!err) {
+          // console.log(res);
+
+          var data = d3.nest()
+                      // .key(function(d) { return d.rec1; })
+                      .entries(res);
+
+          console.log(data);
+
+          var visualization = d3plus.viz()
+            .container("#afectacion-especifica-jm")
+            .background("#EEEEEE")
+            .tooltip(true)
+            .tooltip({"children":0})
+            .data(res)
+            .type("pie")
+            .id(["key"])
+            .size("valor")
+            .format("es_ES")
+            .format({
+                "number": function(number, key) {
+                  var formatted = d3plus.number.format(number, key);
+                  if (key.key === "valor") {
+                      var formatted = number.toLocaleString("es-AR")
+                      return "$" + formatted;
+                  }
+                  else {
+                    return formatted
+                  }
+                }
+            })
+            .draw();
+      }
+      });
+
+
+      $("#afectacion-especifica-propios").empty();
+      d3.json("assets/data/ingresos/recursos-afectacion-propios-presupuesto-2017.json", function(err, res) {
+        if (!err) {
+            // console.log(res);
+
+            var data = d3.nest()
+                        // .key(function(d) { return d.rec1; })
+                        .entries(res);
+
+            console.log(data);
+
+            var visualization = d3plus.viz()
+              .container("#afectacion-especifica-propios")
+              .background("#EEEEEE")
+              .tooltip(true)
+              .tooltip({"children":0})
+              .data(res)
+              .type("bar")
+              .id(["key"])
+              .x("valor")
+              .x({"stacked": false, "value": "valor", "grid":false, "label": false, "scale": "discrete"})
+              .y("Nombre")
+              .y({"scale": "discrete", "grid":false, "label": false}) // Manually set Y-axis to be discrete
+              .format("es_ES")
+              .format({
+                  "number": function(number, key) {
+                    var formatted = d3plus.number.format(number, key);
+                    if (key.key === "valor") {
+                        var formatted = number.toLocaleString("es-AR")
+                        return "$" + formatted;
+                    }
+                    else {
+                      return formatted
+                    }
+                  }
+              })
+              .draw();
+        }
+        });
+        $("#afectacion-especifica-no_propios").empty();
+        d3.json("assets/data/ingresos/recursos-afectacion-no_propios-presupuesto-2017.json", function(err, res) {
+          if (!err) {
+              // console.log(res);
+
+              var data = d3.nest()
+                          // .key(function(d) { return d.rec1; })
+                          .entries(res);
+
+              console.log(data);
+
+              var visualization = d3plus.viz()
+                .container("#afectacion-especifica-no_propios")
+                .background("#EEEEEE")
+                .tooltip(true)
+                .tooltip({"children":0})
+                .data(res)
+                .type("bar")
+                .id(["key"])
+                .x("valor")
+                .x({"scale": "discrete", "stacked": false, "value": "valor", "grid":false, "label": false})
+                .y("Nombre")
+                .y({"scale": "discrete", "grid":false,  "label": false}) // Manually set Y-axis to be discrete
+                .format("es_ES")
+                .format({
+                    "number": function(number, key) {
+                      var formatted = d3plus.number.format(number, key);
+                      if (key.key === "valor") {
+                          var formatted = number.toLocaleString("es-AR")
+                          return "$" + formatted;
+                      }
+                      else {
+                        return formatted
+                      }
+                    }
+                })
+                .draw();
+          }
+          });
+}
+
+
 dibujarD3_ing();
 llenarTablas_ing();
 
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  // e.prevenDefault();
+  if(history.pushState) {
+    history.pushState(null, null, '#'+e.target.hash.substr(1));
+  }
+  else {
+      location.hash = '#'+e.target.hash.substr(1);
+  }
+
+  if(e.target.hash.substr(1) == 'afectacion'){
+    dibujarD3_AE();
+  }
+  return false ;
+})
+
 $(window).on('resize', function(){
   dibujarD3_ing();
+  dibujarD3_AE();
+});
+
+
+$( document ).ready(function() {
+  var hash = window.location.hash;
+  hash = hash.substring(hash.indexOf('#')+1).toLowerCase();
+  console.log(hash);
+  if(hash == "propios" || hash == "no-propios"){
+    $('.nav-tabs a[href="#corrientes"]').tab('show');
+    // $('.nav-tabs a[href="#'+hash+'"]').tab('show');
+  }else if (hash == "cortoplazo" || hash == "largoplazo"){
+    $('.nav-tabs a[href="#capital"]').tab('show');
+    // $('.nav-tabs a[href="#'+hash+'"]').tab('show');
+  }
+  $('.nav-tabs a[href="#'+hash+'"]').tab('show');
+  $('html, body').animate({
+      scrollTop: $("#"+hash).offset().top-275
+    }, 1000);
 });
