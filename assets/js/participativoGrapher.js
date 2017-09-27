cd5f6fb3c9ea2215752c978402d287488085677c
Flor <florenperetti@gmail.com>
2017-09-26 22:30:01 -0300

    Fix issue #27
     - Ahora se dibuja el gráfico con el mismo método que arma la tabla.

--- assets/js/participativoGrapher.js
+++ assets/js/participativoGrapher.js
@@ -1,25 +1,41 @@
-
 window.already_printed_participativo = false;
 
 function dibujarD3_participativo() {
-  //Pestaña Grafico
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1uA9UbORQPEpzDVBpvq4UsmiA0eOvAdFCMg31iXd1XoE/o69vy0y/public/values?alt=json", function( dataJSON ) {
-    $("#participativoGraph").empty();
+  $.getJSON("https://spreadsheets.google.com/feeds/list/1uA9UbORQPEpzDVBpvq4UsmiA0eOvAdFCMg31iXd1XoE/or4ki2f/public/values?alt=json", function( dataJSON ) {
     var datos = [];
-    var totalTodo = 167445426;
-    // console.log(dataJSON.feed);
+    var detalle = []
+    var $tabla = $("#tbody-participativo");
+    $("#participativoGraph").empty();
     $.each( dataJSON.feed.entry, function( key, val ) {
 
-      var concepto = val.gsx$nombre.$t;
-      var total = val.gsx$porcentaje.$t;
-      console.log(parseFloat(total.split(',').join('.')),total,totalTodo * parseFloat(total.split(',').join(''))/100);
-        var linea = {
-          "key": concepto,
-          "valor": (totalTodo * parseFloat(total.split(',').join('')) / 100)}
-        datos.push(linea);
+      var concepto = val.gsx$programa.$t;
+      var porcentaje = val.gsx$porcentaje.$t;
+      var nivel_tabla = val.gsx$nivel.$t;
+      var monto = val.gsx$monto.$t;
+      var nivel_tabla_splited = nivel_tabla.split('.');
+      var nivel = nivel_tabla_splited.length + 1;
+      var texto = val.gsx$texto.$t;
+      detalle[nivel] = concepto.toLowerCase().split(' ').join('_');
 
+      if (nivel_tabla.length == 1 && nivel_tabla != 9) {
+        datos.push({
+          "key": concepto.substr(14,concepto.length-1),
+          "valor": parseInt(val.gsx$monto.$t.split(".").join(""))
+        });
+      }
+
+      if(nivel_tabla == "9"){
+        $tabla.append('<tr class="nivel-1"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+      }else{
+        if(nivel == 3){
+          $tabla.append('<tr class="table-clickable nivel-3" data-toggle="collapse" data-target="#texto-'+detalle[2]+'-'+detalle[3]+'"><td><button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "></i></button></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+          $tabla.append('<tr class="collapse gray" id="texto-'+detalle[2]+'-'+detalle[3]+'"><td></td><td colspan="3">'+texto+'</td></tr>');
+        }else{
+          $tabla.append('<tr class="nivel-'+nivel+'"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+        }
+      }
     });
-    // Grafico Gastos por Clasificacion Economica
+    console.log(datos);
     var visualization = d3plus.viz()
       .container("#participativoGraph")
       .background("#EEEEEE")
@@ -44,39 +60,8 @@ function dibujarD3_participativo() {
           }
       })
       .draw();
-
-    });
-
-    $.getJSON("https://spreadsheets.google.com/feeds/list/1uA9UbORQPEpzDVBpvq4UsmiA0eOvAdFCMg31iXd1XoE/or4ki2f/public/values?alt=json", function( dataJSON2 ) {
-      var datos = [];
-      // console.log(dataJSON2.feed.entry);
-      var detalle = []
-      var $tabla = $("#tbody-participativo");
-      $.each( dataJSON2.feed.entry, function( key, val ) {
-
-        var concepto = val.gsx$programa.$t;
-        var porcentaje = val.gsx$porcentaje.$t;
-        var nivel_tabla = val.gsx$nivel.$t;
-        var monto = val.gsx$monto.$t;
-        var nivel_tabla_splited = nivel_tabla.split('.');
-        var nivel = nivel_tabla_splited.length + 1;
-        var texto = val.gsx$texto.$t;
-        detalle[nivel] = concepto.toLowerCase().split(' ').join('_');
-
-        if(nivel_tabla == "9"){
-          $tabla.append('<tr class="nivel-1"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-        }else{
-          if(nivel == 3){
-            $tabla.append('<tr class="table-clickable nivel-3" data-toggle="collapse" data-target="#texto-'+detalle[2]+'-'+detalle[3]+'"><td><button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "></i></button></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-            $tabla.append('<tr class="collapse gray" id="texto-'+detalle[2]+'-'+detalle[3]+'"><td></td><td colspan="3">'+texto+'</td></tr>');
-          }else{
-            $tabla.append('<tr class="nivel-'+nivel+'"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-          }
-        }
-      });
-      already_printed_participativo=true;
-    });
-  
+    already_printed_participativo=true;
+  });
 }
 
 dibujarD3_participativo();
